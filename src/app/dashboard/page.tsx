4ec5a34b4502719f77e81a7a4e86373105fc0cf3
Joao Mateus <joaomateusmb@gmail.com>
2025-08-13 14:28:59 -0300

    feat: add clicin form page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { db } from "@/db";
 import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
+import SignOutButton from "./components/sign-out-button";
 
 // import SignOutButton from "./components/sign-out-button";
 
@@ -15,18 +16,19 @@ const DashboardPage = async () => {
   if (!session?.user) {
     redirect("/authentication");
   }
-  //   const clinics = await db.query.usersToClinicsTable.findMany({
-  //     where: eq(usersToClinicsTable.userId, session.user.id),
-  //   });
-  //   if (clinics.length === 0) {
-  //     redirect("/clinic-form");
-  //   }
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
       <h1>{session?.user?.name}</h1>
       <h1>{session?.user?.email}</h1>
-      {/* <SignOutButton /> */}
+      <SignOutButton />
     </div>
   );
 };
