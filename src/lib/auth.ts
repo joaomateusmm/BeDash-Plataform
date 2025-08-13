0a690f18fcf11d1be2bbd3a37420a4aa3e6bbd0d
Joao Mateus <joaomateusmb@gmail.com>
2025-08-13 13:25:58 -0300

    feat: sign in and sign up complete

---
+++ src/lib/auth.ts
@@ -0,0 +1,28 @@
+import { db } from "@/db";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+import * as schema from "@/db/schema";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    usePlural: true,
+    schema,
+  }),
+  user: {
+    modelName: "usersTable",
+  },
+  session: {
+    modelName: "sessionsTable",
+  },
+  account: {
+    modelName: "accountsTable",
+  },
+  verification: {
+    modelName: "verificationTable",
+  },
+  emailAndPassword: {
+    enabled: true,
+  },
+});
